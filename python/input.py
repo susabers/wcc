6c5c47d9160ebfbe2c53f6eef681c00280e018c2
susabers <berstler@fas.harvard.edu>
2017-01-07 15:48:15 -0500

    Class

---
+++ python/input.py
@@ -0,0 +1,15 @@
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input('How old are you? ')
+#dog_years = int(age) * 7
+#print('You are ' + str(dog_years) + ' years old in dog years.')
+
+meal_price = float(raw_input('How much was your meal? '))
+tip = int(meal_price) * .20
+total_price = int(meal_price) + (tip)
+print('You should tip $' + str(tip))
+print('Your total cost would be $' + str(total_price))
