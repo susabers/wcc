5fea7005ad1b6032efe08cf3e0e98120d8690cb0
susabers <berstler@fas.harvard.edu>
2017-01-07 15:29:00 -0500

    Class

---
+++ python/variables.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Given variables
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print("Ariel is " + str(age) + " years old now, but she’ll turn " + str(next_age) + " in " + birth_month + ".")
+print("Her birthstone is " + str(birth_stone) + ".")
